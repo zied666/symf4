ca9f1610d26fd9ae8d24ea170c4d2d4d73db6c84
zied666 <zied.kharraz@gmail.com>
2017-12-11 16:13:57 +0100

    update api

--- src/Controller/Api/ApiController.php
+++ src/Controller/Api/ApiController.php
@@ -14,19 +14,12 @@ class ApiController extends Controller
     /**
      * @SWG\Tag(name="Test")
      * @Route("/api/test", methods={"GET"})
-     * @SWG\Response(
-     *     response=200,
-     *     description="Returns the rewards of an user"
-     * )
-     * @SWG\Parameter(
-     *     name="order",
-     *     in="query",
-     *     type="string",
-     *     description="The field used to order rewards"
-     * )
+     * @SWG\Response( response=200,description="Returns the rewards of an user" )
+     * @SWG\Parameter(name="param1", in="query", type="string", description="The field used to order rewards")
+     * @SWG\Parameter(name="param2", in="query", type="string", description="The field used to order rewards")
      */
     public function testAction(Request $request)
     {
-        return $this->json($request->get('order'));
+        return $this->json([$request->get('param1'),$request->get('param2')]);
     }
 }
